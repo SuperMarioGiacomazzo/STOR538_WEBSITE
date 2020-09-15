8d18e6ee07efbba1eeeeae94d9d6a9bf19e902f2
Mario Giacomazzo <mgiacoma@asu.edu>
2020-09-14 22:25:01 -0400

    Index commit

---
+++ Slides/Baseball 6/Runs_to_Wins.R
@@ -0,0 +1,8 @@
+RS=775 #Number of Runs for an Average Team = 775
+personal.Runs.Allowed=10
+RA=775+personal.Runs.Allowed
+Wins=162*RS^2/(RS^2+RA^2)
+print(Wins)
+print(Wins-81) #Number of Wins for an Average Team = 81
+
+
