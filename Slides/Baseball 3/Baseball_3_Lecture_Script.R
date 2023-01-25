d549c3b7bed25245ccefeda1a71ea7014a62915b
Mario Giacomazzo <drmariogiacomazzo@gmail.com>
2023-01-25 12:02:33 -0500

    index commit

--- Slides/Baseball 3/Baseball_3_Lecture_Script.R
+++ Slides/Baseball 3/Baseball_3_Lecture_Script.R
@@ -1,13 +1,39 @@
 #Predicted Runs for Season
 pred.runs.season=function(x){
-  y=x%*%c(-560,.632785531,0.70595,1.2637,1.4907414,0.34656338)
+  y=x%*%c(-422.3215,0.4624,0.8090,1.0566,1.4321,0.3284,0.2045)
   return(y)
 }
 
-pred.runs.season(c(1,1081,486.4,54,810.72139,4359.879477))
+pred.runs.season_book=function(x){
+  y=x%*%c(-422,0.462,0.809,1.056,1.432,0.328,0.204)
+  return(y)
+}
+
+pred.runs.season(c(1,948,285,56,190,513,137)) #Arizona Diamondbacks 2016
 
 #Percent of Outs for Average Players
-3828/4329
+teammult=3912.49/4328.64
+teammult
 
-#Ichiro Added Runs
-839-780
+#Bryant 2016 Stats (S,D,T,HR,BB+HBP,SB)
+bryant16=c(99,35,3,39,93,8)
+
+#Average Team Stats (S,D,T,HR,BB+HBP,SB)
+avg.team=c(939.8286,276.2,29.161905,159.3571,544.5905,95.07619)
+
+#Bryant+Average Team Stats
+bryant.avg=avg.team*teammult+bryant16
+bryant.avg
+
+#Predicted Runs Scored
+avg_predict=pred.runs.season(c(1,avg.team))
+bryant.avg_predict=pred.runs.season(c(1,bryant.avg))
+bryant.avg_predict-avg_predict
+
+#Predicted Runs Scored Book
+avg_predict=pred.runs.season_book(c(1,avg.team))
+bryant.avg_predict=pred.runs.season_book(c(1,bryant.avg))
+bryant.avg_predict-avg_predict
+
+#Slide Check
+0.904*939.83+99
